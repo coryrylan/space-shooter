b1c42620b529c7d1f463c631ae7299174f77050b
Cory Bateman <splintercode.cb@gmail.com>
2015-08-02 12:53:27 -0500

    gulp file clean up and test rename

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 ﻿var gulp = require('gulp');
-var plug = require('gulp-load-plugins')();
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var jscs = require('gulp-jscs');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+
 var babelify = require('babelify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
-var karma = require('karma').server;
 
 var jsLibraries = [
     './app/libraries/requestAnimationFramePolly.js',
@@ -15,41 +21,28 @@ var jsSource = [
     './app/src/**/*.js'
 ];
 
-var specSource = [
-    './specs/*.js'
+var sassSrc = [
+    './styles/**/*.scss'
 ];
 
-var sassSource = [
-    './app/content/sass/**/*.scss'
-];
-
-gulp.task('watch', function() {
-    gulp.watch(sassSource, ['styles']).on('error', catchError);
-    gulp.watch(jsSource, ['hint', 'js', 'tdd']);
-    //gulp.watch(specSource, ['hint', 'js', 'tdd']);
+gulp.task('build.sass', function () {
+    return gulp.src('./styles/app.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .on('error', swallowError)
+        .pipe(sourcemaps.write('./'))
+        .pipe(rename('app.min.css'))
+        .pipe(gulp.dest('build'));
 });
 
-gulp.task('styles', function() {
-    return gulp
-        .src(sassSource)
-        .pipe(plug.rubySass({ style: 'expanded' }))
-        .pipe(plug.autoprefixer('last 2 version', 'ie8', 'ie9'))
-        .pipe(gulp.dest('./build/css'))
-        .pipe(plug.rename({ suffix: '.min' }))
-        .pipe(plug.minifyCss())
-        .pipe(gulp.dest('./build/Css'));
-});
-
-gulp.task('hint', function() {
+gulp.task('js-hint', function() {
     return gulp
         .src(jsSource.concat(specSource))
-        .pipe(plug.jscs({ esnext: true })).on('error', catchError)
-        .pipe(plug.jshint())
-        .pipe(plug.jshint.reporter('jshint-stylish'));
+        .pipe(jscs({ esnext: true })).on('error', swallowError)
+        .pipe(jshint());
 });
 
-gulp.task('js', function() {
-
+gulp.task('build.js', function() {
     // App
     browserify('./app/src/app.js', { debug: true })
         .transform(babelify)
@@ -58,37 +51,21 @@ gulp.task('js', function() {
         .pipe(source('app.js'))
         .pipe(gulp.dest('./build/js'));
 
-    // Specs
-    //browserify('./specs/specs.js', { debug: true })
-    //    .transform(babelify)
-    //    .bundle()
-    //    .on('error', function(err) { console.log('Error: ' + err.message); })
-    //    .pipe(source('specs.js'))
-    //    .pipe(gulp.dest('./build/js'));
-
     // Libraries
     gulp.src(jsLibraries)
-        .pipe(plug.concat('lib.js'))
+        .pipe(concat('lib.js'))
         .pipe(gulp.dest('./build/js'))
-        .pipe(plug.rename({ suffix: '.min' }))
-        .pipe(plug.uglify({ mangle: true }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify({ mangle: true }))
         .pipe(gulp.dest('./build/js'));
 });
 
-gulp.task('test', function (done) {
-    karma.start({
-        configFile: __dirname + '/karma.conf.js',
-        singleRun: true
-    }, done);
+gulp.task('watch', function () {
+    gulp.watch(sassSrc, ['build.sass']);
+    gulp.watch(jsSource, ['js-hint', 'build.js']);
 });
 
-gulp.task('tdd', function (done) {
-    //karma.start({
-    //    configFile: __dirname + '/karma.conf.js'
-    //}, done);
-});
-
-var catchError = function(err) {
-    console.log(err);
+function swallowError(error) {
+    console.log(error.toString());
     this.emit('end');
-};
+}
